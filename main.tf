8dc656b89b13769adc505cac90169c94996a627e
Dhirajpw <dhirajpw92001@gmail.com>
2024-05-17 20:47:30 +0530

    main.tf added

--- main.tf
+++ main.tf
@@ -18,5 +18,42 @@ module "vpc" {
 }
 
  # SG
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "Jenkins-sg"
+  description = "Security group for Jenkins"
+  vpc_id      = module.vpc.vpc_id
+
+    ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+     from_port    = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks [
+    {
+      from_port   = 0 
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags {
+    Name = "Jenkins-sg"
+  }
+}
 
  # EC2
