2e245fc7e605a5b970d67f8f2015330f5265d52f
Dhirajpw <143209173+Dhirajpw@users.noreply.github.com>
2024-05-18 12:08:33 +0530

    Update Jenkins-install.sh

--- Jenkins-install.sh
+++ Jenkins-install.sh
@@ -2,29 +2,22 @@
 
 # install jenkins
 sudo yum update -y
-sudo wget -0/etc/yum.repos.d/jenkins.repo \
+sudo wget -O /etc/yum.repos.d/jenkins.repo \ 
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm -import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
-
- # install java
-sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre -y
-java -version
-
-
 # install git 
-sudo apt install git -y
+sudo yum install git -y
 
 
 # install terraform 
 sudo yum install -y yum-utils
-sudo yum-config-manager--add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
 
@@ -32,4 +25,4 @@ sudo yum -y install terraform
 
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp/kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
