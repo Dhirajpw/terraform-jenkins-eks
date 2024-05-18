d6f9d7ce7444f44aa9fd4ac3c1543678d2826239
Dhirajpw <143209173+Dhirajpw@users.noreply.github.com>
2024-05-18 13:19:13 +0530

    Create jenkins-installation-ubuntu.sh

---
+++ jenkins-installation-ubuntu.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+#install jenkins
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+#install java
+
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+
+#install git
+sudo apt install git -y
+
+#install terraform
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
+
+#install kubectl 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin
