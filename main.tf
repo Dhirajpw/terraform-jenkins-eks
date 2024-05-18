e8dc65f5f671176b8c658b7d16aba6cadd472bea
Dhirajpw <143209173+Dhirajpw@users.noreply.github.com>
2024-05-18 13:20:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ module "ec2_instance" {
   vpc_security_group_ids = [module.sg.security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  user_data              = file("Jenkins-install.sh")
+  user_data              = file("jenkins-installation-ubuntu.sh")
   availability_zone      = var.availability_zone
 
   tags = {
